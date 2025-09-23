58da9e92c940f2b4d75e334047c05ded740d05a6
M-Taghi-Derayati <taghiderayati@live.com>
2025-09-23 19:17:05 +0330

    change evm deposit

--- src/services/TradeExecutionService.ts
+++ src/services/TradeExecutionService.ts
@@ -344,7 +344,7 @@ export class TradeExecutionService {
      */
     public async executeNativeSwapFromEvent(eventData: NativeSwapEventData): Promise<void> {
         // ۱. استخراج و تبدیل داده‌های رویداد
-        const decodedQuoteId = ethers.toUtf8String(eventData.quoteId).replace(/\0/g, '');
+        const decodedQuoteId = ethers.decodeBytes32String(eventData.quoteId);
         const receivedAmount = parseFloat(ethers.formatEther(eventData.amount));
 
         console.log(`[Execute Event] Initiating swap for Quote ID: ${decodedQuoteId} from user ${eventData.user}`);
